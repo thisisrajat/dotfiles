e6dc6b3e99a697a40fbaf97b32f658eecd069b84
Rajat Jain <rajat@hackerrank.com>
2016-11-19 13:57:33 +0530

    Major modifications and added more files

--- brew.sh
+++ brew.sh
@@ -1,105 +1,79 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Install command-line tools using Homebrew.
+# Install command-line tools using Homebrew
 
-# Make sure we’re using the latest Homebrew.
+# Make sure we’re using the latest Homebrew
 brew update
 
-# Upgrade any already-installed formulae.
+# Upgrade any already-installed formulae
 brew upgrade
 
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
 
-# Install some other useful utilities like `sponge`.
+# GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
 brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+# GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
 brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
+# GNU `sed`, overwriting the built-in `sed`
 brew install gnu-sed --with-default-names
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
+
+
+# Bash 4
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
 brew install bash
 brew tap homebrew/versions
 brew install bash-completion2
 
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
-fi;
+brew install homebrew/completions/brew-cask-completion
 
-# Install `wget` with IRI support.
+# generic colouriser  http://kassiopeia.juls.savba.sk/~garabik/software/grc/
+brew install grc
+
+# Install wget with IRI support
 brew install wget --with-iri
 
-# Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
-# see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
-
-# Install more recent versions of some macOS tools.
-brew install vim --override-system-vi
+# Install more recent versions of some OS X tools
+brew install vim --with-override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
-brew install homebrew/php/php56 --with-gmp
 
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+# run this script when this file changes guy.
+brew install entr
 
-# Install other useful binaries.
-brew install ack
-brew install dark-mode
-#brew install exiv2
+# github util. imho better than hub
+brew install gh
+
+
+# mtr - ping & traceroute. best.
+brew install mtr
+
+    # allow mtr to run without sudo
+    mtrlocation=$(brew info mtr | grep Cellar | sed -e 's/ (.*//') #  e.g. `/Users/paulirish/.homebrew/Cellar/mtr/0.86`
+    sudo chmod 4755 $mtrlocation/sbin/mtr
+    sudo chown root $mtrlocation/sbin/mtr
+
+
+# Install other useful binaries
+brew install the_silver_searcher
+brew install fzf
+
 brew install git
-brew install git-lfs
 brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
+brew install node # This installs `npm` too using the recommended installation method
 brew install pv
 brew install rename
-brew install rhino
-brew install speedtest_cli
-brew install ssh-copy-id
-brew install testssl
 brew install tree
-brew install vbindiff
-brew install webkit2png
 brew install zopfli
+brew install ffmpeg --with-libvpx
 
-# Remove outdated versions from the cellar.
+brew install terminal-notifier
+
+brew install android-platform-tools
+brew install pidcat   # colored logcat guy
+
+brew install zsh
+
+# Remove outdated versions from the cellar
 brew cleanup
